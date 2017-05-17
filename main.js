e99bf790647d09204976ed9216df46f4bf1cfea9
Monte9 <Monte9@users.noreply.github.com>
2017-05-16 21:24:25 -0700

    added simple bg images on touch

--- main.js
+++ main.js
@@ -1,24 +1,64 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const IMAGES = [
+  {
+    source: require('./src/images/wallpaper_1.jpg')
+  },
+  {
+    source: require('./src/images/wallpaper_2.jpg')
+  },
+  {
+    source: require('./src/images/wallpaper_3.jpg')
+  },
+  {
+    source: require('./src/images/wallpaper_4.jpg')
+  },
+]
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      index: 0
+    }
+  }
+
+  renderNextImage() {
+    const { index } = this.state
+
+    this.setState({
+      index: (index + 1) % 4
+    })
+  }
+
   render() {
+    const { index } = this.state
+
     return (
-      <View style={styles.container}>
-        <Text>Yo</Text>
-      </View>
+      <TouchableOpacity style={styles.container} activeOpacity={1} onPress={this.renderNextImage.bind(this)}>
+        <Image style={styles.bgImage} source={IMAGES[index].source} />
+      </TouchableOpacity>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  bgImage: {
+    height: SCREEN_HEIGHT,
+    width: SCREEN_WIDTH,
+    position: 'absolute',
+    top: 0,
+    left: 0
+  }
 });
 
 Expo.registerRootComponent(App);
