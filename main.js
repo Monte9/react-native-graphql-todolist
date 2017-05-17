0b432668937680cf7fd087c99eb4a4a3f0d2e836
Monte9 <Monte9@users.noreply.github.com>
2017-05-16 23:05:08 -0700

    listView UI complete

--- main.js
+++ main.js
@@ -2,13 +2,76 @@ import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 
-import { Icon } from 'react-native-elements'
+import { Icon, List, ListItem } from 'react-native-elements'
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
+const LIST = [
+  {
+    description: 'Buy groceries on my way home',
+    category: 'Home',
+    notes: "Don't forget or wifey won't make you any dinner",
+  },
+  {
+    description: 'Finish React Native GraphQL Todo List App',
+    category: 'Projects',
+    notes: 'GraphQL is :fire:',
+  },
+  {
+    description: 'Release v3.1',
+    category: 'Work',
+    notes: 'Need to codepush it. QA it before release',
+  },
+]
+
+const COLORS = {
+  titleGreyInComplete: 'rgba(0, 0, 0, 1)',
+  titleGreyComplete: 'rgba(117, 117, 119, 1)',
+  subtitleGreyInComplete: 'rgba(117, 117, 119, 1)',
+  subtitleGreyComplete: 'rgba(172, 171, 173, 1)',
+  checkmarkGreen: 'rgba(131, 175, 41, 1)',
+  checkmarkBlack: 'rgba(0, 0, 0, 1)'
+}
+
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      checked: false
+    }
+  }
+
+  itemCompleteIconStyle() {
+    return {
+      name: 'ios-checkmark-circle',
+      type: 'ionicon',
+      size: 30,
+      color: COLORS.checkmarkGreen
+    }
+  }
+
+  itemInCompleteIconStyle() {
+    return {
+      name: 'ios-radio-button-off',
+      type: 'ionicon',
+      size: 30,
+      color: COLORS.checkmarkBlack
+    }
+  }
+
+  onListItemPress() {
+    const { checked } = this.state
+
+    this.setState({
+      checked: !checked
+    })
+  }
+
   render() {
+    const { checked } = this.state
+
     return (
       <View style={styles.container}>
         <Image style={styles.bgImage} source={require('./src/images/top_wallpaper.png')} />
@@ -35,6 +98,22 @@ class App extends React.Component {
           </View>
         </View>
         <View style={styles.listView}>
+          <List containerStyle={styles.listContainerView}>
+            {
+              LIST.map((item, index) => (
+                <ListItem
+                  key={index}
+                  hideChevron
+                  title={item.description}
+                  titleStyle={checked ? styles.itemCompleteTitle : styles.itemInCompleteTitle}
+                  subtitle={item.category}
+                  subtitleStyle={checked ? styles.itemCompleteSubtitle : styles.itemInCompleteSubtitle}
+                  leftIcon={checked ? this.itemCompleteIconStyle() : this.itemInCompleteIconStyle()}
+                  onPress={this.onListItemPress.bind(this)}
+                />
+              ))
+            }
+          </List>
         </View>
         <View style={styles.addNewItemView}>
         </View>
@@ -87,6 +166,29 @@ const styles = StyleSheet.create({
   listView: {
     flex: 7,
     backgroundColor: 'rgba(247,247, 250, 1)'
+  },
+  listContainerView: {
+    marginTop: -3
+  },
+  itemCompleteTitle: {
+    fontSize: 17,
+    color: COLORS.titleGreyComplete,
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid'
+  },
+  itemInCompleteTitle: {
+    fontSize: 17,
+    color: COLORS.titleGreyInComplete,
+  },
+  itemCompleteSubtitle: {
+    fontSize: 14,
+    color: COLORS.subtitleGreyComplete,
+    fontWeight: '400'
+  },
+  itemInCompleteSubtitle: {
+    fontSize: 14,
+    color: COLORS.subtitleGreyInComplete,
+    fontWeight: '400'
   },
   addNewItemView: {
     position: 'absolute',
